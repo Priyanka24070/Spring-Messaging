14b64d59cdc212dbb53dccadd03a17ad7694da06
Priyanka. <priyankakumarixy0070@gmail.com>
2025-02-22 13:00:24 +0530

    [Priyanka] added rest call to show hello mark taylor

--- HelloWorld/src/main/java/com/example/HelloWorld/HelloController.java
+++ HelloWorld/src/main/java/com/example/HelloWorld/HelloController.java
@@ -26,6 +26,11 @@ class HelloController {
         return "Hello " + user.getFirstName() + " " + user.getLastName() + " from BridgeLabz";
     }
 
+    @PutMapping("hello/put/{firstName}/")
+    public String sayHello(@PathVariable String firstName, @RequestParam String lastName) {
+        return "Hello " + firstName + " " + lastName + " from BridgeLabz";
+    }
+
 }
 
 

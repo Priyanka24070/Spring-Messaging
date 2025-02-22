e2e90b36da5504316411431fe66efb1969b95b89
Priyanka. <priyankakumarixy0070@gmail.com>
2025-02-22 12:46:38 +0530

    [Priyanka]added rest call to show hello

--- HelloWorld/src/main/java/com/example/HelloWorld/HelloController.java
+++ HelloWorld/src/main/java/com/example/HelloWorld/HelloController.java
@@ -9,10 +9,25 @@ class HelloController {
     public String sayHello() {
         return "<h1>Hello from BridgeLabz</h1>";
     }
-    @RequestMapping(value = {"/query"},method = RequestMethod.GET)
-    public String sayHello(@RequestParam(value="name")String name) {
+
+    @RequestMapping(value = {"/query"}, method = RequestMethod.GET)
+    public String sayHello(@RequestParam(value = "name") String name) {
         return "Hello" + name + "!";
     }
+
+    // Make REST Call to show Hello {name} from BridgeLabz as path variable
+    @GetMapping("/hello/param/{name}")
+    public String hello(@PathVariable String name) {
+        return "Hello " + name + " from Bridgelabz";
+    }
+
+    @PostMapping("/hello/post")
+    public String greetUser(@RequestBody User user) {
+        return "Hello " + user.getFirstName() + " " + user.getLastName() + " from BridgeLabz";
+    }
+
 }
+
+
 
 
